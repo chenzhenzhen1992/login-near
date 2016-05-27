c0756c32ec4889bcd5a89bf3be887cf58eea6e03
zhenzhen <839108635@qq.com>
2016-05-27 11:17:12 +0800

    push the app

---
+++ README.md
@@ -0,0 +1,2 @@
+# login-near
+  a partial function from os(occupational server) base on android client, Ruby on Rails for server.
---
+++ login-near client/os/gen/com/example/css/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.css;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
